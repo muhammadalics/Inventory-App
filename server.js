2b83d94d1986ce1e5258c665d326e136f6f0a69c
Muhammad Ali <muhammadali.pete@gmail.com>
2021-02-21 19:49:12 -0700

    trying different connection code.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const mongoose = require('mongodb').MongoClient;
 const url = 'mongodb://localhost/inventorydb' 
-mongoose.connect("mongodb+srv://testuser404:404400200@cluster0.us3q5.mongodb.net/wild-fjord?retryWrites=true&w=majority", {useNewUrlParser:true})
+mongoose.connect("mongodb+srv://testuser404:404400200@cluster0.us3q5.mongodb.net/wild-fjord?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true})
 const con = mongoose.connection
 con.on('open', () => 
 {console.log('connected!')})
