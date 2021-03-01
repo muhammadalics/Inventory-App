393170af07ddc289093b3abb23fdb9b0a6213730
Muhammad Ali <muhammadali.pete@gmail.com>
2021-03-01 11:59:53 -0700

    PUT fixed.

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 const mongoose = require('mongoose');
 //const url = "mongodb+srv://testuser404:404400200@cluster0.us3q5.mongodb.net/wild-fjord?retryWrites=true&w=majority"
 const url = "mongodb://localhost/"
-mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 const con = mongoose.connection
-con.on('open', () => {console.log('connected!')})
+con.on('open', () => { console.log('connected!') })
 
 // const MongoClient = require('mongodb').MongoClient;
 // const uri = "mongodb+srv://testuser404:404400200@cluster0.us3q5.mongodb.net/wild-fjord?retryWrites=true&w=majority";
@@ -33,6 +33,7 @@ const Inventory = require('./inventory')
 
 const model = require('./model')
 const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
 
 // const port = process.env.port 
 
@@ -43,15 +44,15 @@ app.use(bodyParser.raw());
 
 
 //GET all products-
-app.get('/api/products', async (req, res)=>{
-    const products = await Inventory.find()    
+app.get('/api/products', async (req, res) => {
+    const products = await Inventory.find()
     res.json(products)
     //res.send('Hello World!!!')
 })
 
 
 //GET a product by id
-app.get('/api/products/:id', async (req, res)=>{
+app.get('/api/products/:id', async (req, res) => {
     const product = await Inventory.findById(req.params.id)
     res.send(`Found the product: ${product}`)
 
@@ -59,7 +60,7 @@ app.get('/api/products/:id', async (req, res)=>{
 
 
 //POST a new product
-app.post('/api/products', async (req, res)=>{   
+app.post('/api/products', async (req, res) => {
     const product = new Inventory({
         department: req.body.department,
         name: req.body.name,
@@ -70,10 +71,10 @@ app.post('/api/products', async (req, res)=>{
 
     })
 
-    try{
+    try {
         const newProduct = await product.save()
         res.json(newProduct)
-    }catch(err){
+    } catch (err) {
         res.send("Error: Could notsave new product");
         throw err;
     }
@@ -96,13 +97,13 @@ app.post('/api/products', async (req, res)=>{
 // app.get('/api/products', async (req, res)=>{
 //     const products = await model.getAllProducts()
 //     res.send(products)
- 
+
 // })
 
 //Get a product by ID
 // app.get('/api/products/:id', async (req, res)=>{   
 //     const product = await model.getProductById(req.params.id)
-    
+
 //     if (!product){
 //         res.status(404).send('Not Found')
 //     }   
@@ -126,21 +127,21 @@ app.post('/api/products', async (req, res)=>{
 
 
 //Delete a product by id.
-app.delete('/api/products/:id', async (req, res)=>{
+app.delete('/api/products/:id', async (req, res) => {
     const product = await Inventory.findById(req.params.id)
 
     console.log(product)
-    if (!product){
+    if (!product) {
         res.status(404).send('Not Found')
-    }   
-    else{        
+    }
+    else {
         res.send(product)
         // model.deleteProductById(product)
-        Inventory.deleteOne({_id: req.params.id}, function (err) {
+        Inventory.deleteOne({ _id: req.params.id }, function (err) {
             if (err) return handleError(err);
             // deleted at most one tank document
-          });
-    }   
+        });
+    }
 })
 
 
@@ -166,24 +167,68 @@ app.delete('/api/products/:id', async (req, res)=>{
 
 
 
-//update a new product
-app.put('/api/products', async (req, res)=>{   
-    const body = req.body    
-    const product = await model.getProductById(body.id)
+// //update a new product
+// app.put('/api/products', async (req, res)=>{   
+//     const body = req.body    
+//     const product = await model.getProductById(body.id)
 
-    if (!product){
+//     if (!product){
+//         res.status(404).send("Product not found.")
+//     }   
+//     else{        
+//         const product = await model.getProductById(body.id)
+//         model.deleteProductById(product)
+//         model.postProduct(req.body)
+//         res.status(200).send("Product updated!")
+//     }   
+// })
+
+
+
+//update a product
+app.put('/api/products', async (req, res) => {
+    const body = req.body
+    console.log("Body:")
+    console.log(body)
+    const product = await Inventory.findById(body.id)
+
+    if (!product) {
         res.status(404).send("Product not found.")
-    }   
-    else{        
-        const product = await model.getProductById(body.id)
-        model.deleteProductById(product)
-        model.postProduct(req.body)
+    }
+    else {
+        var x = new mongoose.Types.ObjectId(body.id);
+        console.log('This is body.id:' +  typeof(body.id));
+        console.log('This is x:' +  typeof(x));
+        try {
+            await Inventory.update(
+                { _id: x },
+
+                {
+                    department: body.department,
+                    "name": body.name,
+                    "material": body.material,
+                    "weight": parseFloat(body.weight),
+                    "corrosionfree": body.corrosionfree,
+                    "size": body.size
+
+                }
+
+            )
+        }
+        catch (err) {
+            throw err;
+        }
+
+        const product = await Inventory.findById(body.id)
+        console.log(product)
+
         res.status(200).send("Product updated!")
-    }   
+    }
 })
 
-app.listen(PORT, ()=>{
+
+
+
+app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
-
-
