5292394f01f783970431eda92f43a97227cead4a
Muhammad Ali <muhammadali.pete@gmail.com>
2021-03-03 20:36:36 -0700

    updating whitelist

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ con.on('open', () => { console.log('connected!') })
 
 //https://daveceddia.com/access-control-allow-origin-cors-errors-in-react-express/
 // Set up a whitelist and check against it:
-var whitelist = ['https://muhammadalics.github.io/React-Inventory-frontend', 'https://muhammadalics.github.io/']
+var whitelist = ['https://muhammadalics.github.io/React-Inventory-frontend', 'https://muhammadalics.github.io/', 
+                  'https://muhammadalics.github.io/React-Inventory-frontend/']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
