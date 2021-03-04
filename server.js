b624bcbb94866afa34d9d94a1f448cf02d75da07
Muhammad Ali <muhammadali.pete@gmail.com>
2021-03-03 20:56:54 -0700

    cors removed.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT | 5000;
 const express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 const app = express();
 
 //https://daveceddia.com/access-control-allow-origin-cors-errors-in-react-express/
@@ -20,7 +20,7 @@ const app = express();
 // // Then pass them to cors:
 // app.use(cors(corsOptions));
 
-app.use(cors());
+// app.use(cors());
 
 
 
